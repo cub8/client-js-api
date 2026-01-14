68a0942c59fa1612e87fd6e75a459be2f83a55eb
cub8 <tsubait8@gmail.com>
2026-01-14 20:49:05 +0100

    Improve seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -30,6 +30,50 @@ async function createClients() {
     pesel: "00000000000",
     status: "RESIGNED",
   })
+
+  await clientFactory.create({
+    firstName: "Ignacy",
+    lastName: "Paderewski",
+    pesel: "13874187487",
+    status: "INTEGRATED",
+    integrations: [{ type: "INTERNAL" }, { type: "API" }],
+  })
+
+  await clientFactory.create({
+    firstName: "Jan",
+    lastName: "Nowak",
+    pesel: "18417498189",
+    status: "INTEGRATED",
+    integrations: [{ type: "INTERNAL" }, { type: "API" }],
+  })
+
+  await clientFactory.create({
+    firstName: "Karol",
+    lastName: "Nowak",
+    pesel: "98189471481",
+    status: "REGISTERED",
+  })
+
+  await clientFactory.create({
+    firstName: "Anna",
+    lastName: "Weksel",
+    pesel: "09089578749",
+    status: "REGISTERED",
+  })
+
+  await clientFactory.create({
+    firstName: "Bartłomiej",
+    lastName: "Weksel",
+    pesel: "74878418471",
+    status: "RESIGNED",
+  })
+
+  await clientFactory.create({
+    firstName: "Harold",
+    lastName: "Bryne",
+    pesel: "34134131311",
+    status: "RESIGNED",
+  })
 }
 
 export async function main() {
